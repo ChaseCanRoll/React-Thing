d6baa8e11b2d5c491ee537329c9c6f73cac56f25
Code Success <codesuccess@cwi.edu>
2021-07-19 10:36:55 -0600

    initial Commit

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,8 @@ import PortfolioContainer from "./portfolio/portfolio-container";
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
@@ -20,8 +22,10 @@ export default class App extends Component {
             <NavigationContainer />
 
             <Switch>
-              <Route exact path="/" Component={Home} />
-              <Route path="/about-me" Component={About} />
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
             </Switch>
           </div>
         </Router>
