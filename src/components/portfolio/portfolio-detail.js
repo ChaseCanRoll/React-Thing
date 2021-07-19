2e3c170d812d1f66b22f34a6b004ac128b85a936
Code Success <codesuccess@cwi.edu>
2021-07-19 11:16:41 -0600

    initial Commit

---
+++ src/components/portfolio/portfolio-detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ProgressPlugin } from 'webpack';
+
+export default function() {
+    return (
+        <div>
+            <h2>Portfolio Detail for {Props.match.params.slug}</h2>
+        </div>
+    );
+}
